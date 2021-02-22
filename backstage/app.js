1c163e61db3c68c6bd85ca1e5e04da5e30db3160
fanningyuan <fanningyuan@jd.com>
2021-02-22 11:27:13 +0800

    add:我喜欢的音乐

--- backstage/app.js
+++ backstage/app.js
@@ -3,10 +3,12 @@ var app = express();
 const Login = require('./src/login');
 const Music = require('./src/music');
 const Register = require('./src/register');
+const Like = require('./src/Ilike');
 
 app.use('/login',Login);
 app.use('/home',Music);
 app.use('/register',Register)
+app.use('/like',Like);
 app.listen(80, () => {
     console.log('web server running at http://127.0.0.1');
 })
--- backstage/src/login.js
+++ backstage/src/login.js
@@ -9,27 +9,23 @@ var db = '';
 app.use(bodyParser.urlencoded({extended:false}))
 app.post('/',(req,res)=>{
     var isLogin = false;
+    var id = '';
     connection.query(sql,(err,results)=>{
         if(err){
             console.log(err.message);
             return;
         }
-        console.log(req.body.username);
-        console.log(req.body.password);
         for(let i = 0;i<results.length;i++){
-            
             if(results[i].user_name == req.body.username && results[i].pass_word == req.body.password){
                 isLogin = true;
-                console.log(results[i].user_name);
-                console.log(results[i].pass_word);
-                console.log(isLogin);
+                id = results[i].id;
                 break;
             }
         }
         if(!isLogin){
-            db = { state: 200, message: '登陆失败', content: isLogin }
+            db = { state: 200, message: '登陆失败', content: isLogin, id: id };
         }else{
-            db = { state: 200, message: '登陆成功', content: isLogin };
+            db = { state: 200, message: '登陆成功', content: isLogin, id: id };
         }
         res.send(db); 
     })
