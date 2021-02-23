cd23b1dab126e67eca08dfd6c42178f871113fba
fanningyuan <fanningyuan@jd.com>
2021-02-23 14:43:58 +0800

    feat:流式播放器,好像好用了

--- backstage/src/mplay.js
+++ backstage/src/mplay.js
@@ -5,35 +5,14 @@ const fs = require("fs");
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 
-function strToBinary(str){
-    var result = [];
-    var list = str.split("");
-    for(var i=0;i<list.length;i++){
-        if(i !== 0){
-            result.push(" ");
-        }
-        var item = list[i];
-        var binaryStr = item.charCodeAt().toString(2);
-        result.push(binaryStr);
-    }
-    return result.join("");
-}
 app.post('/', (req, res) => {
-    let data = '';
     let sql = "SELECT * FROM music WHERE m_id = '" + req.body.musicid + "'";
   connection.query(sql,(err,results)=>{
     if(err){
         console.log(err.message)
     }
     let readerStream = fs.createReadStream(''+results[0].m_dirname);
-    readerStream.setEncoding('UTF8');
-    readerStream.on('data',(chunk)=>{
-        data += chunk;
-    })
-    readerStream.on('end',()=>{
-        console.log(strToBinary(data))
-        res.send(strToBinary(data));
-    })
+    readerStream.pipe(res)
   })
 });
 module.exports = app;
